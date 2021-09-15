d8406a466614fcbf35cf1e36be891d25e43db289
carey036 <xsnet36@gmail.com>
2021-09-15 21:41:01 +0800

    Create web.sh

---
+++ release/web.sh
@@ -0,0 +1,7 @@
+wget https://carey036.github.io/modify_magisk_for_k40g/index.html
+mkdir modify_magisk_for_k40g
+mkdir modify_magisk_for_k40g/assets
+mkdir modify_magisk_for_k40g/assets/images
+mkdir modify_magisk_for_k40g/assets/css
+wget -O modify_magisk_for_k40g/assets/images/blacktocat.png https://carey036.github.io/modify_magisk_for_k40g/assets/images/blacktocat.png
+wget -O modify_magisk_for_k40g/assets/css/style.css https://carey036.github.io/modify_magisk_for_k40g/assets/css/style.css?v=451986ea8921cdf93e182810b791241e39cfdcd0
