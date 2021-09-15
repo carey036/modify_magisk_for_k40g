b9f251c7b04d07f0bb13798a69f0ac6d74f4a67b
carey036 <xsnet36@gmail.com>
2021-09-15 21:47:03 +0800

    Update web.sh

--- release/web.sh
+++ release/web.sh
@@ -5,3 +5,4 @@ mkdir modify_magisk_for_k40g/assets/images
 mkdir modify_magisk_for_k40g/assets/css
 wget -O modify_magisk_for_k40g/assets/images/blacktocat.png https://carey036.github.io/modify_magisk_for_k40g/assets/images/blacktocat.png
 wget -O modify_magisk_for_k40g/assets/css/style.css https://carey036.github.io/modify_magisk_for_k40g/assets/css/style.css?v=451986ea8921cdf93e182810b791241e39cfdcd0
+wget -O modify_magisk_for_k40g/zanshang.jpg https://carey036.github.io/modify_magisk_for_k40g/zanshang.jpg
