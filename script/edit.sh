6139756205a0e5c5f792e3da8a45cd3aa191f490
carey036 <xsnet36@gmail.com>
2021-09-16 08:14:55 +0800

    change patch method

---
+++ script/edit.sh
@@ -0,0 +1,39 @@
+sed -i '/# Sign chromeos boot/ i ui_print "- rebuild for k40g酷安@响當當"\n.\/magiskboot hexpatch new-boot.img "0300000000617662746F6F6C20" "0000000000617662746F6F6C20"' ./scripts/boot_patch.sh
+sed -i 's/主页/主页(k40g n10p)/' ./app/src/main/res/values-zh-rCN/strings.xml
+wget -O magisk.jks https://cdn.jsdelivr.net/gh/carey036/modify_magisk_for_k40g/script/magisk.jks
+sed -i '/keyStore/d' config.prop.sample
+sed -i '/keyStorePass/d' config.prop.sample
+sed -i '/keyPass/d' config.prop.sample
+sed -i '/keyAlias/d' config.prop.sample
+sed -i '/outdir/d' config.prop.sample
+sed -i '/version/d' config.prop.sample
+echo "version="`git log -1 --format='%h'` >> config.prop.sample
+echo "outdir=out" >> config.prop.sample
+echo "keyStore=./magisk.jks" >> config.prop.sample
+echo "keyStorePass=magisk" >> config.prop.sample
+echo "keyAlias=xingsuo" >> config.prop.sample
+echo "keyPass=magisk" >> config.prop.sample
+cat config.prop.sample > config.prop
+cat>>./app/src/main/res/raw/manager.sh<<EOF
+restore_imgs() {
+[ -z \$SHA1 ] && return 1
+local BACKUPDIR=/data/magisk_backup_\$SHA1
+[ -d \$BACKUPDIR ] || return 1
+
+get_flags
+find_boot_image
+
+for name in dtb dtbo; do
+    [ -f \$BACKUPDIR/\${name}.img.gz ] || continue
+    local IMAGE=\$(find_block \$name\$SLOT)
+    [ -z \$IMAGE ] && continue
+    flash_image \$BACKUPDIR/\${name}.img.gz \$IMAGE
+done
+[ -f \$BACKUPDIR/boot.img.gz ] || return 1
+rm \$BOOTIMAGE
+gzip -d \$BACKUPDIR/boot.img.gz
+dd if=\$BACKUPDIR/boot.img of=\$BOOTIMAGE
+gzip \$BACKUPDIR/boot.img
+#flash_image \$BACKUPDIR/boot.img.gz \$BOOTIMAGE && rm -rf \$BACKUPDIR
+}
+EOF
