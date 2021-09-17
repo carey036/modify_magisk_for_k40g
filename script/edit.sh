76cae8d9304459081870942f76225497ac940359
carey036 <xsnet36@gmail.com>
2021-09-17 08:04:48 +0800

    Update edit.sh

--- script/edit.sh
+++ script/edit.sh
@@ -39,8 +39,9 @@ gzip \$BACKUPDIR/boot.img
 }
 EOF
 #try to add channel
-sed -i "s/const val STABLE_CHANNEL = 0/const val XS = 5\nconst val STABLE_CHANNEL = 0" app/src/main/java/com/topjohnwu/magisk/core/Config.kt
-sed -i "s/const val CUSTOM_CHANNEL = \"custom_channel\"/const val XS = \"k40\"const val CUSTOM_CHANNEL = \"custom_channel\"/" app/src/main/java/com/topjohnwu/magisk/core/Config.kt
+sed -i "s/const val STABLE_CHANNEL = 0/const val XS = 5\n        const val STABLE_CHANNEL = 0" app/src/main/java/com/topjohnwu/magisk/core/Config.kt
+sed -i "s/const val CUSTOM_CHANNEL = \"custom_channel\"/const val XS = \"k40\"\n        const val CUSTOM_CHANNEL = \"custom_channel\"/" app/src/main/java/com/topjohnwu/magisk/core/Config.kt
+cat app/src/main/java/com/topjohnwu/magisk/core/Config.kt
 sed -i "s/<item>Alpha<\/item>/<item>Alpha<\/item>\n<item>XS<\/item>/" app/src/main/res/values/arrays.xml
 sed -i "s/import com.topjohnwu.magisk.core.Config.Value.CUSTOM_CHANNEL/import com.topjohnwu.magisk.core.Config.Value.XS\n            import com.topjohnwu.magisk.core.Config.Value.CUSTOM_CHANNEL/" app/src/main/java/com/topjohnwu/magisk/data/repository/NetworkService.kt
 sed -i "s/CUSTOM_CHANNEL -> fetchCustomUpdate(Config.customChannelUrl)/XS -> fetchCustomUpdate(\"https:\/\/magisk.pages.dev\/alpha.json\")\nCUSTOM_CHANNEL -> fetchCustomUpdate(Config.customChannelUrl)/" app/src/main/java/com/topjohnwu/magisk/data/repository/NetworkService.kt
